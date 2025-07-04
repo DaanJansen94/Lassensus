69c0f63812aaacf2685509a8bfb10a709c8f4267
DaanJansen94 <jansendaan94@gmail.com>
2025-07-04 17:44:15 +0200

    Bump version to 0.0.3
    - Smart rarefaction with --max_reads parameter
    - Support for both .fastq and .fastq.gz files
    - Fixed read counting for uncompressed files
    - NumPy compatibility constraint for medaka
    - Updated README with all optional arguments
    - Cleaned up dependencies

--- lassensus/__init__.py
+++ lassensus/__init__.py
@@ -4,5 +4,5 @@ Lassensus - Lassa virus consensus sequence builder
 A tool for generating consensus sequences from Lassa virus sequencing data.
 """
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __author__ = "Daan Jansen" 
