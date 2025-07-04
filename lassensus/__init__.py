df527dc77bef63ce8c10615da198d5aa4dc1f2a8
DaanJansen94 <jansendaan94@gmail.com>
2025-07-04 14:57:36 +0200

    .

---
+++ lassensus/__init__.py
@@ -0,0 +1,8 @@
+"""
+Lassensus - Lassa virus consensus sequence builder
+
+A tool for generating consensus sequences from Lassa virus sequencing data.
+"""
+
+__version__ = "0.0.2"
+__author__ = "Daan Jansen" 
